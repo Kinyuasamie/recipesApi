89587461f4ef0d23fa7aa7cb4ea1b81a21e8483c
kinyuasamie <samiekinyua322@gmail.com>
2024-07-16 20:34:47 +0300

    recipes

---
+++ android/app/src/main/kotlin/com/example/recipeapi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.recipeapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:recipeapi/views/home.dart';
+//import 'package:food_recipe/views/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food recipe',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
